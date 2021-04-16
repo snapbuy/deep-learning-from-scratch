15ce9b545cb2cec135dc4ef570bea0ea08238228
snapbuy <snapbuy@naver.com>
2021-04-16 11:58:20 +0900

    path

--- ch01/img_show.py
+++ ch01/img_show.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
-img = imread('dataset\cactus.png') # 이미지 읽어오기
+img = imread('ch01/images/fig 1-1.png') # 이미지 읽어오기
 plt.imshow(img)
 
 plt.show()
