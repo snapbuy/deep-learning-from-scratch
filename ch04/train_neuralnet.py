6e9562cef097a0280d2437db698d9ad3b4525992
Jason Cho <snapbuy@naver.com>
2022-08-28 17:04:46 +0900

    etc

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -3,6 +3,11 @@ import sys, os
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import matplotlib.pyplot as plt
+
+sys.path.append("./")
+
+
+
 from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
