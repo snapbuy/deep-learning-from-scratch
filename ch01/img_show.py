2144145ecc3226d498eceae92accba65da0028fc
Jason Cho <snapbuy@naver.com>
2025-09-30 08:44:30 +0900

    img_show.py

--- ch01/img_show.py
+++ ch01/img_show.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
-img = imread('ch01/images/fig 1-1.png') # 이미지 읽어오기
+img = imread('images/fig 1-1.png') # 이미지 읽어오기
 plt.imshow(img)
 
 plt.show()
